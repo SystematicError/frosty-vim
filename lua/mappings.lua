0788097b2b4042673e03c1ea7f9355b1b777d725
SystematicError <systematicerror@users.noreply.github.com>
2023-12-30 19:21:24 +0000

    Add some keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Leader>t", "<Cmd>Neotree toggle<CR>")
 vim.keymap.set("n", "<Leader>l", "<Cmd>Lazy<CR>")
+
+vim.keymap.set("n", "<Leader> ", "<Cmd>Neotree toggle<CR>")
+
+vim.keymap.set("n", "<Leader>tf", "<Cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<Leader>tg", "<Cmd>Telescope live_grep<CR>")
