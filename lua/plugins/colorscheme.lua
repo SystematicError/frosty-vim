985b815fccfcea2e423918089737cdeb7b23caff
SystematicError <systematicerror@users.noreply.github.com>
2024-01-02 07:41:52 +0000

    feat: add accent colors to colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -24,27 +24,23 @@ local config = {
             surface2 = "#616161",
             surface1 = "#4c4c4c",
             surface0 = "#373737",
+            rosewater = "#edcbcb",
+            flamingo = "#e8c4b4",
+            pink = "#f9b3c0",
+            mauve = "#8d8bc4",
+            red = "#fc4e5e",
+            maroon = "#f76271",
+            peach = "#ffaf60",
+            yellow = "#f6cd7e",
+            green = "#bbef6e",
+            teal = "#96e0c9",
+            sky = "#b8e5ec",
+            sapphire = "#75beef",
+            blue = "#5baaea",
+            lavender = "#bdace0",
         },
     },
 }
-
---[[
-    rosewater = "#f5e0dc"
-    flamingo = "#f2cdcd"
-    pink = "#f5c2e7"
-    mauve = "#cba6f7"
-    red = "#f38ba8"
-    maroon = "#eba0ac"
-    peach = "#fab387"
-    yellow = "#f9e2af"
-    green = "#a6e3a1"
-    teal = "#94e2d5"
-    sky = "#89dceb"
-    sapphire = "#74c7ec"
-    blue = "#89b4fa"
-    lavender = "#b4befe"
-]]
---
 
 return {
     {
