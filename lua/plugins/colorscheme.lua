1efb7d988a31a60c8b1174efd65809c23b2e437a
SystematicError <systematicerror@users.noreply.github.com>
2024-01-05 16:02:00 +0000

    feat: telescope borderless look

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -58,6 +58,18 @@ local function config()
                 NeoTreeWinSeparator = { fg = colors.mantle, bg = colors.mantle },
                 NeoTreeCursorLine = { bg = colors.mantle },
                 NeoTreeIndentMarker = { fg = colors.surface0 },
+
+                TelescopeSelection = { bg = colors.crust },
+                TelescopePromptNormal = { bg = colors.crust },
+                TelescopePromptBorder = { fg = colors.crust, bg = colors.crust },
+                TelescopePromptTitle = { fg = colors.crust, bg = colors.blue, style = { "bold" } },
+                TelescopePromptPrefix = { fg = colors.overlay0 },
+                TelescopePreviewNormal = { bg = colors.mantle },
+                TelescopePreviewBorder = { fg = colors.mantle, bg = colors.mantle },
+                TelescopePreviewTitle = { fg = colors.mantle },
+                TelescopeResultsNormal = { bg = colors.mantle },
+                TelescopeResultsBorder = { fg = colors.mantle, bg = colors.mantle },
+                TelescopeResultsTitle = { fg = colors.mantle },
             }
         end,
     }
