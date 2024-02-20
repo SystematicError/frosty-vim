9c5985196f271a50a1608cc1cd435cf563d4ad12
SystematicError <systematicerror@users.noreply.github.com>
2024-02-20 11:49:46 +0000

    feat: download lazy on non nix systems

--- init.lua
+++ init.lua
@@ -1,5 +1,23 @@
 FROSTY_PACKAGES = FROSTY_PACKAGES or {}
-vim.opt.rtp:prepend(FROSTY_PACKAGES["folke/lazy.nvim"] or vim.fn.stdpath "data" .. "/lazy/lazy.nvim")
+
+if FROSTY_PACKAGES["folke/lazy.nvim"] then
+    vim.opt.rtp:prepend(FROSTY_PACKAGES["folke/lazy.nvim"])
+else
+    local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable",
+            lazypath,
+        }
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+end
 
 local function ensure_table(object)
     return type(object) == "table" and object or { object }
