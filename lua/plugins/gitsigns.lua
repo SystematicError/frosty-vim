c682a47ad2901deff0c9999e780cbd01db967cbc
SystematicError <systematicerror@users.noreply.github.com>
2024-03-30 13:24:17 +0000

    feat: format line blame nicer

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,8 @@ local function config()
             changedelete = { text = "_" },
             untracked = { text = "┆" },
         },
+
+        current_line_blame_formatter = "  <author>, <author_time:%Y-%m-%d> • <summary>",
     }
 
     require("gitsigns").setup(opts)
