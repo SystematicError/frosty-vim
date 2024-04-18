d72da380d3f1963d1857df52ba1930300cf6621c
SystematicError <systematicerror@users.noreply.github.com>
2024-04-18 18:31:59 +0000

    style: format treesitter module

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,9 @@
 local function config()
     local opts = {
-        -- HACK: Prevents treesitter complaining about readonly nix store
-        -- Use flake to install treesitter parsers instead
         ensure_installed = {},
 
-        highlight = {
-            enable = true,
-        },
-
-        indent = {
-            enable = true,
-        },
+        highlight = { enable = true },
+        indent = { enable = true },
     }
 
     require("nvim-treesitter.configs").setup(opts)
