aee24096072180589dcafae0881cd1a2861f4e2c
SystematicError <systematicerror@users.noreply.github.com>
2024-04-24 13:30:19 +0000

    feat: customise dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,6 +1,32 @@
+local function config()
+    local opts = {
+        theme = "doom",
+
+        config = {
+            center = {
+                { icon = "", desc = "Find files", key = "f", action = "Telescope find_files" },
+                { icon = "", desc = "File tree", key = "t", action = "Neotree toggle" },
+                { icon = "", desc = "New file", key = "n", action = "enew | startinsert" },
+                { icon = "", desc = "Recent files", key = "r", action = "Telescope oldfiles" },
+                { icon = "󰦨", desc = "Live grep", key = "g", action = "Telescope live_grep" },
+                { icon = "", desc = "Quit", "hi", key = "q", action = "qa" },
+            },
+
+            footer = {},
+        },
+    }
+
+    for _, button in ipairs(opts.config.center) do
+        button.icon = button.icon .. "  "
+        button.desc = button.desc .. string.rep(" ", 40 - #button.desc)
+    end
+
+    require("dashboard").setup(opts)
+end
+
 return {
     {
         "nvimdev/dashboard-nvim",
-        config = true,
+        config = config,
     },
 }
