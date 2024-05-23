0f01e839b3808c9d96208ed15d655d9e8d9b6cc2
SystematicError <systematicerror@users.noreply.github.com>
2024-05-23 20:35:48 +0000

    feat: add split navigation mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,6 +2,11 @@ return {
     n = {
         ["<c-u>"] = { "<c-u>zz", "Scroll up" },
         ["<c-d>"] = { "<c-d>zz", "Scroll down" },
+
+        ["<c-h>"] = { "<c-w>h", "Move to left split" },
+        ["<c-j>"] = { "<c-w>j", "Move to below split" },
+        ["<c-k>"] = { "<c-w>k", "Move to above split" },
+        ["<c-l>"] = { "<c-w>l", "Move to right split" },
     },
 
     v = {
