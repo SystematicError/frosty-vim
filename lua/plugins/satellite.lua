1ab65a6d73ab8a34a8309a1c165e3484137314c5
SystematicError <systematicerror@users.noreply.github.com>
2025-03-06 17:05:02 +0000

    feat: configure satellite

--- lua/plugins/satellite.lua
+++ lua/plugins/satellite.lua
@@ -1,5 +1,26 @@
+local function config()
+    local git_icons = require("icons").git_gutter
+
+    local opts = {
+        current_only = true,
+
+        excluded_filetypes = {
+            "snacks_dashboard",
+            "neo-tree",
+        },
+
+        handlers = {
+            gitsigns = {
+                signs = git_icons,
+            },
+        },
+    }
+
+    require("satellite").setup(opts)
+end
+
 return {
     "lewis6991/satellite.nvim",
     lazy = false,
-    config = true,
+    config = config,
 }
