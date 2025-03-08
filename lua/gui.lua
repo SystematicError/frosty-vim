7e73ef02fba6595657b07290b3f32e8cc1694e1b
SystematicError <systematicerror@users.noreply.github.com>
2025-03-08 06:41:40 +0000

    feat: neovide fullscreen support

--- lua/gui.lua
+++ lua/gui.lua
@@ -2,4 +2,8 @@ if vim.g.neovide then
     vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
     vim.g.neovide_floating_corner_radius = 0.25
     vim.g.neovide_refresh_rate = 144
+
+    vim.keymap.set("n", "<f11>", function()
+        vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+    end, { desc = "Toggle fullscreen" })
 end
