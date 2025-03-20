c12048a4b6eb52b387a19e7b53a984ca4155761f
SystematicError <systematicerror@users.noreply.github.com>
2025-03-20 16:20:13 +0000

    feat: add confirmation before quitting without saving
    also sorted the option list

--- lua/options.lua
+++ lua/options.lua
@@ -1,37 +1,27 @@
 local opt = vim.opt
 
-opt.termguicolors = true
-
-opt.lazyredraw = true
-
-opt.timeoutlen = 500
-
-opt.wrap = false
-opt.whichwrap = "bshl<>[]~"
-
-opt.mouse = "a"
-
-opt.signcolumn = "yes:1"
-opt.fillchars = { eob = " " }
-
-opt.showmode = false
-opt.ruler = false
-opt.laststatus = 3
 opt.cmdheight = 0
-
-opt.scrolloff = 4
-
-opt.splitbelow = true
-opt.splitright = true
-
+opt.confirm = true
+opt.expandtab = true
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.laststatus = 3
+opt.lazyredraw = true
+opt.mouse = "a"
 opt.number = true
 opt.relativenumber = true
-
-opt.expandtab = true
-opt.smartindent = true
-opt.tabstop = 4
-opt.softtabstop = 4
+opt.ruler = false
+opt.scrolloff = 4
 opt.shiftwidth = 4
-
-opt.ignorecase = true
+opt.showmode = false
+opt.signcolumn = "yes:1"
 opt.smartcase = true
+opt.smartindent = true
+opt.softtabstop = 4
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop = 4
+opt.termguicolors = true
+opt.timeoutlen = 500
+opt.whichwrap = "bshl<>[]~"
+opt.wrap = false
