e7f019d9ea5226431c7cdd9a81279336922da076
SystematicError <systematicerror@users.noreply.github.com>
2025-05-20 20:00:51 +0000

    refactor: modularise lightbulb opts table

--- lua/plugins/lightbulb.lua
+++ lua/plugins/lightbulb.lua
@@ -1,30 +1,29 @@
--- TODO: Modularise opts table
 -- TODO: Modularise snacks integration
 -- TODO: Lazy load lightbulb
 
-local function config()
-    local bulb_icons = require("icons").lightbulb
+local bulb_icons = require("icons").lightbulb
 
+local default_opts = {
+    code_lenses = true,
+
+    autocmd = { enabled = true },
+
+    sign = { enabled = false },
+
+    virtual_text = {
+        enabled = true,
+
+        text = bulb_icons.action,
+        lens_text = bulb_icons.lens,
+    },
+
+    filter = function()
+        return vim.g.enable_lightbulb
+    end,
+}
+
+local function config(_, opts)
     vim.g.enable_lightbulb = true
-
-    local opts = {
-        code_lenses = true,
-
-        autocmd = { enabled = true },
-
-        sign = { enabled = false },
-
-        virtual_text = {
-            enabled = true,
-
-            text = bulb_icons.action,
-            lens_text = bulb_icons.lens,
-        },
-
-        filter = function()
-            return vim.g.enable_lightbulb
-        end,
-    }
 
     if Snacks then
         Snacks.toggle({
@@ -43,6 +42,9 @@ end
 
 return {
     "kosayoda/nvim-lightbulb",
+
     lazy = false,
+
+    opts = default_opts,
     config = config,
 }
