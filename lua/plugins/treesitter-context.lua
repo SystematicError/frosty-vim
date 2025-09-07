5c5a41e6cbacdf890733b81d0e3eb2a97d7763b9
SystematicError <systematicerror@users.noreply.github.com>
2025-09-07 17:08:32 +0000

    feat: add keybind to jump to context

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -10,6 +10,15 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
 
     lazy = false,
+    keys = {
+        {
+            "[c",
+            function()
+                require("treesitter-context").go_to_context(vim.v.count1)
+            end,
+            desc = "Previous context",
+        },
+    },
 
     opts = default_opts,
 
